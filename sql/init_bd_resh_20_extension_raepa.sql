f2debe3b1db0965d68e83a6e2638017923b12046
fvanhoutte <37404404+fvanhoutte@users.noreply.github.com>
2019-04-05 11:51:53 +0200

    Update init_bd_resh_20_extension_raepa.sql

--- sql/init_bd_resh_20_extension_raepa.sql
+++ sql/init_bd_resh_20_extension_raepa.sql
@@ -1,9 +1,9 @@
-/*RAEPA V1.1*/
+/*RAEPA V1.2*/
 /*Revision et extension locale du squelette de la structure des données (table, séquence, trigger,...) du standard RAEPA */
 /*init_bd_resh_20_extension_raepa.sql */
 /*PostGIS*/
 
-/* Propriétaire : GeoCompiegnois - http://geo.compiegnois.fr/ */
+/* PGeoCompiegnois - http://geo.compiegnois.fr/ */
 /* Auteur : Florent Vanhoutte */
 
 
