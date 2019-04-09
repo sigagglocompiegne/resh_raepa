1bdd25ca5b5036a4e8332c034d040517213f7b66
fvanhoutte <37404404+fvanhoutte@users.noreply.github.com>
2019-04-09 12:49:52 +0200

    Update init_bd_resh_20_extension_raepa.sql

--- sql/init_bd_resh_20_extension_raepa.sql
+++ sql/init_bd_resh_20_extension_raepa.sql
@@ -284,60 +284,6 @@ COMMENT ON COLUMN m_raepa.geo_raepa_noeud.angle IS 'Angle en degré décimaux ut
 
 
 
--- ####################################################################################################################################################
--- ###                                                                                                                                              ###
--- ###                                                                        VUES                                                                  ###
--- ###                                                                                                                                              ###
--- ####################################################################################################################################################
-
-
--- #################################################################### VUE CANALISATION AEP ###############################################
-        
--- View: m_raepa.geo_v_raepa_canalaep_l
-
--- DROP VIEW m_raepa.geo_v_raepa_canalaep_l;
-
-CREATE OR REPLACE VIEW m_raepa.geo_v_raepa_canalaep_l AS 
- SELECT 
-  g.idcana,
-  g.mouvrage,
-  g.gexploit, 
-  g.enservice,
-  g.branchemnt,
-  g.materiau,
-  g.materiau2,
-  g.diametre,
-  g.forme,  
-  g.anfinpose,
-  g.modecircu,
-  a.contcanaep,
-  a.fonccanaep,     
-  g.idnini,
-  g.idnterm,
-  g.idcanppale,
-  g.zgensup,
-  a.profgen,
-  g.andebpose,
-  g.longcana,
-  g.nbranche,
-  m.qualglocxy,
-  m.qualglocz, 
-  m.datemaj,
-  m.sourmaj,
-  m.qualannee,
-  m.dategeoloc,
-  m.sourgeoloc,
-  m.sourattrib,
-  g.geom
-  
-FROM m_raepa.geo_raepa_canal g
-RIGHT JOIN m_raepa.an_raepa_canalaep a ON g.idcana = a.idcana
-RIGHT JOIN m_raepa.an_raepa_metadonnees m ON g.idcana = m.idraepa
-ORDER BY g.idcana;
-
-COMMENT ON VIEW m_raepa.geo_v_raepa_canalaep_l
-  IS 'Canalisation d''adduction d''eau';
-
 /*
 
 
